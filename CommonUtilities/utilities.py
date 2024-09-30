4c1ebc49cca668da42739dd06b8d3c9e304b8901
Hetu <sahu.hetram@gmail.com>
2024-09-30 08:43:25 +0530

    usage of common utitlities

---
+++ CommonUtilities/utilities.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import pytest
+from sqlalchemy import create_engine
+import logging
+import cx_Oracle
+
+
+# Configure the logging
+from CommonUtilities.config import *
+logging.basicConfig(
+    filename='Logs/etlTestAutomation.log',  # Name of the log file
+    filemode='a',        # 'a' to append, 'w' to overwrite
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Log format
+    level=logging.INFO    # Set the logging level
+)
+logger = logging.getLogger(__name__)
+
+
+# create mysql database commection
+mysql_engine = create_engine(f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}')
+# Create Oracle engine
+oracle_engine = create_engine(f'oracle+cx_oracle://{ORACLE_USER}:{ORACLE_PASSWORD}@{ORACLE_HOST}:{ORACLE_PORT}/{ORACLE_SERVICE}')
+
+
+def file_to_db_verify(file_path,table_name,db_engine,file_type):
+    if file_type == "csv":
+        expected_df = pd.read_csv(file_path)
+    elif file_type == "xml":
+        expected_df = pd.read_xml(file_path,xpath = './/item')
+    elif file_type == "json":
+        expected_df = pd.read_json(file_path)
+    else:
+        raise ValueError(f"unsupported file type : {file_type}")
+
+    query = f"select * from {table_name};"
+    actual_df = pd.read_sql(query, db_engine)
+    assert actual_df.equals(expected_df), f"Data extraction failed."
+    print("Sales data extarcted and laoded to staging successfuly")
+
+def db_to_db_verify(query1,db_engine1,query2,db_engine2,):
+    actual_df = pd.read_sql(query1,db_engine1)
+    expected_df = pd.read_sql(query2, db_engine2)
+    assert actual_df.equals(expected_df), f"Data extraction failed."
--- TestScripts/test_extract.py
+++ TestScripts/test_extract.py
@@ -1,15 +1,45 @@
 import pandas as pd
 import pytest
-from sqlalchemy import create_engine,text
-import cx_Oracle
-# Create MySQL database connection
-mysql_engine = create_engine('mysql+pymysql://root:Admin%40143@localhost:3308/enterpriseretaildwh')
+from sqlalchemy import create_engine
+import logging
 
-oracle_engine = create_engine('oracle+cx_oracle://system:admin@localhost:1521/xe')
 
-def test_sales_data_Extraction():
-    expected_df = pd.read_csv("TestData/sales_data.csv")
-    query = "select * from staging_sales"
-    actual_df = pd.read_sql(query, mysql_engine)
-    assert actual_df.equals(expected_df), f"Data extraction failed."
-    print("Sales data extarcted and laoded to staging successfuly")
+# Configure the logging
+from CommonUtilities.config import *
+from CommonUtilities.utilities import file_to_db_verify, db_to_db_verify
+
+logging.basicConfig(
+    filename='Logs/etlTestAutomation.log',  # Name of the log file
+    filemode='a',        # 'a' to append, 'w' to overwrite
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Log format
+    level=logging.INFO    # Set the logging level
+)
+logger = logging.getLogger(__name__)
+
+
+# create mysql database commection
+mysql_engine = create_engine(f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}')
+# Create Oracle engine
+oracle_engine = create_engine(f'oracle+cx_oracle://{ORACLE_USER}:{ORACLE_PASSWORD}@{ORACLE_HOST}:{ORACLE_PORT}/{ORACLE_SERVICE}')
+
+
+
+def test_dataExtrcation_from_Sales_data_File():
+    file_to_db_verify("TestData/sales_data.csv","staging_sales",mysql_engine,'csv')
+
+
+def test_dataExtrcation_from_product_data_File():
+    file_to_db_verify("TestData/product_data.csv","staging_product",mysql_engine,'csv')
+
+def test_dataExtrcation_from_inventory_data_File():
+    file_to_db_verify("TestData/inventory_data.xml","staging_inventory",mysql_engine,'xml')
+
+def test_dataExtrcation_from_supplier_data_File():
+    file_to_db_verify("TestData/supplier_data.json", "staging_supplier", mysql_engine,'json')
+
+
+def test_dataExtrcation_from_stores_oracle_table():
+    query1 = """select * from stores"""
+    query2 = """select * from staging_store"""
+    db_to_db_verify(query1,oracle_engine,query2,mysql_engine)
+
